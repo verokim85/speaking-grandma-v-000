a5d92b9cf3a94a35e8f291b366110823f8691ed5
Veronica Kim <vero.a.kim@gmail.com>
2018-01-18 02:54:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 def speak_to_grandma(phrase)
-  if "#{phrase}" = "#{phrase}".uppercase
+  if phrase = phrase.uppercase
     return "NO, NOT SINCE 1938!"
-  elsif "#{phrase}" == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
