3ecab06f867bd036e75806529e7d2b5a1229a5e4
Veronica Kim <vero.a.kim@gmail.com>
2018-01-18 02:59:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,8 +7,6 @@ def speak_to_grandma(phrase)
   return "I LOVE YOU TOO PUMPKIN!"
     if phrase = phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
